7fceb9f7621dad16609e4fcc1ece64db4b2e88fa
sanoz77 <marlessondograu@gmail.com>
2026-03-12 14:48:19 -0300

    primeira versão do painel escudo driver

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1-Y4aFdGYWOayGFETbi9PNO6pp04hSB8",
+  authDomain: "escudo-driver.firebaseapp.com",
+  databaseURL: "https://escudo-driver-default-rtdb.firebaseio.com",
+  projectId: "escudo-driver",
+  storageBucket: "escudo-driver.firebasestorage.app",
+  messagingSenderId: "470154175918",
+  appId: "1:470154175918:web:0cc928d7877135028f943f"
+};
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
