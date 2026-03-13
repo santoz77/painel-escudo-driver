8ea5b05588940044b9dea3b7c273324cf20622cb
sanoz77 <marlessondograu@gmail.com>
2026-03-13 00:45:09 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -268,18 +268,7 @@ export default function App() {
     await signOut(auth);
   }
 
-  async function uploadImage(file: File) {
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", UPLOAD_PRESET);
 
-    const res = await fetch(
-      `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
-      {
-        method: "POST",
-        body: formData,
-      }
-    );
 
     if (!res.ok) {
       throw new Error("Erro ao enviar imagem");
